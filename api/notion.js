1ef512ba3d0d2ce3e8cf810b7f3736e0c43a6b8c
JUPITERBYRD <thisisjupiterbyrd@gmail.com>
2025-08-19 21:49:16 +0200

    Utilising Notion Blocks API

--- api/notion.js
+++ api/notion.js
@@ -7,7 +7,11 @@ export default async function handler(req, res) {
 
     const pageId = req.query.pageId || process.env.NOTION_PAGE_ID;
 
-    const page = await notion.pages.retrieve({ page_id: pageId });
+    //const page = await notion.pages.retrieve({ page_id: pageId });
+    const response = await notion.blocks.children.list({
+      block_id: pageId,
+    });
+    console.log(response);
 
     res.status(200).json(page);
   } catch (err) {
