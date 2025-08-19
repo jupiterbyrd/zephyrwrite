8296afa1ad3b552125adf469bb7ec0f98d5e1a48
JUPITERBYRD <thisisjupiterbyrd@gmail.com>
2025-08-19 17:15:48 +0200

    Cleaning up notion.js

--- api/notion.js
+++ api/notion.js
@@ -1,19 +1,17 @@
-import "dotenv/config";
-const { Client } = require("@notionhq/client");
-
-const notion = new Client({
-  auth: process.env.NOTION_API_KEY,
-});
+// /api/notion.js
+import { Client } from "@notionhq/client";
 
 export default async function handler(req, res) {
-  const { pageId } = req.query;
-
   try {
-    console.log("HOOO");
+    const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+    const pageId = req.query.pageId || process.env.NOTION_PAGE_ID;
+
     const page = await notion.pages.retrieve({ page_id: pageId });
-    console.log("HAAA");
+
     res.status(200).json(page);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
   }
 }
