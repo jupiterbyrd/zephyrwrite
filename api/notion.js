55fd0801d6be6b97dd9feded0665b4d04e0afd63
JUPITERBYRD <thisisjupiterbyrd@gmail.com>
2025-08-19 22:03:12 +0200

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -5,12 +5,13 @@ export default async function handler(req, res) {
   try {
     const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-    const pageId = req.pageId;
+    const pageId = req.query.pageId;
 
     const page = await notion.pages.retrieve({ page_id: pageId });
-    /*const response = await notion.blocks.children.list({
+    console.log(page);
+    const response = await notion.blocks.children.list({
       block_id: pageId,
-    });*/
+    });
     console.log(response);
 
     res.status(200).json(page);
