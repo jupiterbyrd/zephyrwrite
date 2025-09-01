ddacba16085299c916555488bec3e269d950a885
JUPITERBYRD <thisisjupiterbyrd@gmail.com>
2025-09-01 19:55:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import "./styles/styles.css";
 import { getArticles, getPageContent } from "./lib/notion.js";
 import { analyzeText } from "./lib/readability.js";
